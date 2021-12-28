242fe48880de7a6b68e7d4ed457fde5fe9d21ffd
sachinphatak24 <phatak.sachin12@gmail.com>
2021-12-28 17:45:53 +0530

    maybe restore

--- models/user.js
+++ models/user.js
@@ -6,6 +6,6 @@ const userSchema = mongoose.Schema({
     password:{type:String,required:true},
     type:{type:String, required:true},
     id:{type:String}
-}) 
+})
 
 export default mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { signin , signup } from '../controllers/user.js';
+import { signin , signup , adminSignup} from '../controllers/user.js';
 
 
-router.post("/signin",signin);
+
+router.post("/admin/signup",adminSignup);
+
+
 router.post("/signup",signup);
 
+router.post("/signin",signin);
 
 export default router;
