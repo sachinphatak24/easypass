e330bc285cdd3756925d33aad77357d2c5f7642c
sachinphatak24 <phatak.sachin12@gmail.com>
2022-01-03 03:26:08 +0530

    added verification for user profiles

--- models/profile.js
+++ models/profile.js
@@ -4,6 +4,7 @@ import User from './user.js';
 const profileSchema = mongoose.Schema({
     user:{type: mongoose.Schema.Types.ObjectId,ref:'User'},
     // usertype:{type:String, default:'Student'},
+    email:{type:String,ref:'User',required:true},
     nameAsPerIdCard:{type:String,required:true},
     dateOfBirth:{type:String,required:true},
     collegeId:{type: String, required:true},
@@ -12,6 +13,8 @@ const profileSchema = mongoose.Schema({
     branchName:{type:String,required:true},
     location:{type:String},
     date:{type:Date, default: Date.now},
+    profileVerifyApplied:{ type: Boolean, default: false },
+    profileVerifystatus:{type:String,default:"UnVerified"},
     passinfo:[{
         passType:{type:String,default:''},
         passVerifyStatus:{type:String,default:'UnVerified'},
--- routes/profile.js
+++ routes/profile.js
@@ -3,14 +3,20 @@ const router = express.Router();
 
 import authenticate from '../middleware/authentication.js';
 
-import { allProfiles , createProfile, currentProfile, verifyProfile} from '../controllers/profiles.js';
+import { allProfiles , createProfile, currentProfile, verifyProfile, adminverifyProfile, adminverifyProfilee} from '../controllers/profiles.js';
 
 
 //All Profiles Route (Hidden:Admins)
 router.get('/all',authenticate, allProfiles);
 
-// Verify Current User's Profile(Hidden:Current User)
-router.get('/verify',authenticate, verifyProfile);
+//Apply to Verify Current User's Profile(Hidden:Current User)
+router.post('/verify',authenticate, verifyProfile);
+
+// Check Users Profiles For Verification(Hidden:collegeAdmin)
+router.get('/adminverify',authenticate, adminverifyProfile);
+
+// Confirm Verification of Users(Hidden:collegeAdmin)
+router.post('/adminverify',authenticate, adminverifyProfilee);
 
 // Create or Update Current User's Profile(Hidden:Current User)
 router.post('/create', authenticate, createProfile);
