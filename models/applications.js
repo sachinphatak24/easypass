18558806f06fc99d5424368566d5d2565c3f87e0
sachin <phatak.sachin12@gmail.com>
2022-05-05 13:20:54 +0530

    small change

--- models/applications.js
+++ models/applications.js
@@ -1,25 +1,25 @@
-import  mongoose from 'mongoose';
+// import  mongoose from 'mongoose';
 
-import User from './user.js';
+// import User from './user.js';
 
-import profile from './profile.js';
+// import profile from './profile.js';
 
-const applicationSchema = mongoose.Schema({
-    profile:{type:mongoose.Schema.Types.ObjectId, ref:'Profile'},
-    email:{type:String},
-    collegeName:{type:String},
-    applications:[
-        {
-            travelOption:{type:String},
-            startLocation:{type:String},
-            endLocation:{type:String},
-            travelPassPeriod:{type:String},
-            applicationStatus:{type:String},
-            appliedOn:{type:String},
-            addressProof:{type:String},
-            applicationAcceptedOn:{type:String}
-        } 
-    ]
-})
+// const applicationSchema = mongoose.Schema({
+//     profile:{type:mongoose.Schema.Types.ObjectId, ref:'Profile'},
+//     email:{type:String},
+//     collegeName:{type:String},
+//     applications:[
+//         {
+//             travelOption:{type:String},
+//             startLocation:{type:String},
+//             endLocation:{type:String},
+//             travelPassPeriod:{type:String},
+//             applicationStatus:{type:String},
+//             appliedOn:{type:String},
+//             addressProof:{type:String},
+//             applicationAcceptedOn:{type:String}
+//         } 
+//     ]
+// })
 
-export default mongoose.model('applications', applicationSchema);
+// export default mongoose.model('applications', applicationSchema);
