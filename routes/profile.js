bf5b551a28732d65e954eb793369d8570d9789d0
sachin <phatak.sachin12@gmail.com>
2022-05-21 19:57:40 +0530

    razorpayapi test no.7

--- routes/profile.js
+++ routes/profile.js
@@ -56,14 +56,17 @@ router.post('/paymentVerify',authenticate, async(req,res) => {
         if (razorpay_signature === expectedSign) {
             Profile.findOneAndUpdate(
                 // {email:req.body.email,'applications.currentApplication.applicationStatus':"Under Process"},
-                {email:req.userInfo.email},
+                {user:req.userId},
                 // {email:req.body.email},
-                {$set:{'applications.currentApplication.amountPaid':true,'applications.currentApplication.paymentId':razorpay_payment_id,'applications.allApplications.$.amountPaid':true,'applications.allApplications.$.paymentId':razorpay_payment_id,'applications.allApplications.$.paymentPaidOn':Date().toString(),'applications.currentApplication.paymentPaidOn':Date().toString()}},
+                // {$set:{'applications.currentApplication.amountPaid':true,'applications.currentApplication.paymentId':razorpay_payment_id,'applications.allApplications.$.amountPaid':true,'applications.allApplications.$.paymentId':razorpay_payment_id,'applications.allApplications.$.paymentPaidOn':Date().toString(),'applications.currentApplication.paymentPaidOn':Date().toString()}},
+                {$set:{'applications.currentApplication.amountPaid':true}},
                 // {$set:{'applications.allApplications.$.applicationStatus':"calm"}},
                 {new: true}
-                )
-                const response = await Profile.findOne({email:req.userInfo.email});
-                return res.json({status:200,response,message:"Payment Verified Successfully!"});
+                ).then( async() => {   
+                    const response = await Profile.findOne({user:req.userId});
+                    return res.json({status:200,response,message:"Payment Verified Successfully!"});
+                }
+                    )
         } else {
             return res.json({status:400, message:"Invalid Signature Sent!"});
         }
