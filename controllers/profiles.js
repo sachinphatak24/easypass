4afcc9ee615c28b7c2c4ef9610edd1e9095c4220
sachin <phatak.sachin12@gmail.com>
2022-05-30 00:29:17 +0530

    big changes part 9

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -623,7 +623,7 @@ export const adminRejectProfilee = async(req,res) => {
                         const verifiedProfiles = await Profile.find({profileVerifyApplied:true, collegeName:req.userInfo.collegeName,profileVerifystatus:'Verified'});
                         const userType = req.userInfo.type;
                         if(userType ==='college admin'){
-                            const deletedP = await Profile.deleteOne({user:req.userId});
+                            const deletedP = await Profile.deleteOne({email:req.body.email});
                             console.log(deletedP);
                            res.json({status:'Successfully Rejected!',unVerifiedProfiles,verifiedProfiles});
                         }else{
